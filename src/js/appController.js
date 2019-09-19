aa710cb995873d69a6a981387a3b598c844c4549
Nedu Robert <nedumrobert@gmail.com>
2019-09-19 14:53:40 +0100

    feat: added styling

--- src/js/appController.js
+++ src/js/appController.js
@@ -28,7 +28,7 @@ define(['knockout', 'ojs/ojmodule-element-utils', 'ojs/ojknockouttemplateutils',
        // Router setup
        self.router = Router.rootInstance;
        self.router.configure({
-         'signup': { label: 'Signup', isDefault: true},
+         'register': { label: 'Register', isDefault: true},
          'login': { label: 'Login'},
          'dashboard': {label: 'Dashboard'}
        });
@@ -55,7 +55,7 @@ define(['knockout', 'ojs/ojmodule-element-utils', 'ojs/ojknockouttemplateutils',
 
       // Navigation setup
       var navData = [
-      {name: 'Register', id: 'signup'},
+      {name: 'Register', id: 'register'},
       {name: 'Login', id: 'login'},
       {name: 'Dashboard', id: 'dashboard',
        iconClass: 'oj-navigationlist-item-icon demo-icon-font-24 demo-chart-icon-24'},
